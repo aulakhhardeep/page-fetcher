d52808181accb89359e5b079a2d622aebfd2381a
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-29 15:55:35 -0600

    Add program logic

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,23 @@
 const request = require('request');
 const fs = require('fs');
+let urlOfWebsite = process.argv[2];
+let localPath = process.argv[3];
 
-request('http://www.example.edu', (error, response, body) => {
-  console.log('error:', error);//Print the error if one occured
+request(urlOfWebsite, (error, response, body) => {
+  if(error) {
+    console.log('error:', error);//Print the error if one occured
+    return; //stop running
+  }
+  fs.writeFile(localPath, body, (error) => {
+    if (error) {
+      console.log('error: ', error);
+      return;
+    }
+    console.log("Downloaded and saved 3261 bytes to ./index.html");
+  });
+ 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
-fs.writeFile('out.txt', 'This should get written for file', (err => {
-  console.log("This printed after the file is written");
-}))
 
-console.log("Hello");
+
